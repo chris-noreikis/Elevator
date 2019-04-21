2f4335cc3070b84bbdd70efb7a36eb95bf950bd8
Shatilla Prayer <shatilla.prayer@unlv.edu>
2019-04-21 13:19:19 -0700

    testing the UI with the elevators

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,70 +1,80 @@
 
 package main;
+import gui.ElevatorDisplay;
+import gui.ElevatorDisplay.Direction;
+import models.*;
 
+/**
+ * @author Shatilla
+ *
+ */
 public class Main {
     static int personCounter = 0;
 
-    public static void main(String[] args) throws InterruptedException {
+ /*   public static void main(String[] args) throws InterruptedException {
         ElevatorTestDriver d = new ElevatorTestDriver();
         d.runTests();
+    }*/
+
+        public static void main(String[] args) throws InterruptedException {
+        
+        int numFloor = 20;
+        int numElev = 4;
+        
+        ElevatorDisplay.getInstance().initialize(numFloor);
+        for (int i = 1; i <= numElev; i++) {
+            ElevatorDisplay.getInstance().addElevator(i, 1);
+        }
+
+        // Go up
+        for (int j = 1; j <= numElev; j++) {
+            moveElevator(j, 1, numFloor);
+            ElevatorDisplay.getInstance().setIdle(j);
+        }
+        Thread.sleep(1000);
+
+        // Go down
+        for (int j = 1; j <= numElev; j++) {
+            moveElevator(j, numFloor, 1);
+            ElevatorDisplay.getInstance().setIdle(j);
+        }
+        Thread.sleep(1000);
+
+        // Go up halfway
+        moveElevator(1, 1, numFloor / 2);
+        Thread.sleep(1000);
+
+        // Go up remaining floors
+        moveElevator(1, numFloor / 2, numFloor);
+        Thread.sleep(1000);
+
+        for (int i = numFloor; i > 1; i--) {
+            moveElevator(1, i, i - 1);
+            Thread.sleep(500);
+        }
+        ElevatorDisplay.getInstance().setIdle(1);
+        Thread.sleep(1000);
+
+        ElevatorDisplay.getInstance().shutdown();
     }
 
-//        public static void main(String[] args) throws InterruptedException {
-//
-//        int numFloor = 20;
-//        int numElev = 4;
-//        ElevatorDisplay.getInstance().initialize(numFloor);
-//        for (int i = 1; i <= numElev; i++) {
-//            ElevatorDisplay.getInstance().addElevator(i, 1);
-//        }
-//
-//        // Go up
-//        for (int j = 1; j <= numElev; j++) {
-//            moveElevator(j, 1, numFloor);
-//            ElevatorDisplay.getInstance().setIdle(j);
-//        }
-//        Thread.sleep(1000);
-//
-//        // Go down
-//        for (int j = 1; j <= numElev; j++) {
-//            moveElevator(j, numFloor, 1);
-//            ElevatorDisplay.getInstance().setIdle(j);
-//        }
-//        Thread.sleep(1000);
-//
-//        // Go up halfway
-//        moveElevator(1, 1, numFloor / 2);
-//        Thread.sleep(1000);
-//
-//        // Go up remaining floors
-//        moveElevator(1, numFloor / 2, numFloor);
-//        Thread.sleep(1000);
-//
-//        for (int i = numFloor; i > 1; i--) {
-//            moveElevator(1, i, i - 1);
-//            Thread.sleep(500);
-//        }
-//        ElevatorDisplay.getInstance().setIdle(1);
-//        Thread.sleep(1000);
-//
-//        ElevatorDisplay.getInstance().shutdown();
-//    }
-//
-//    private static void moveElevator(int elevNum, int fromFloor, int toFloor) throws InterruptedException {
-//        int numRiders = (int) (11.0 * Math.random()) + 1;
-//
-//        ElevatorDisplay.getInstance().closeDoors(elevNum);
-//        if (fromFloor < toFloor) {
-//            for (int i = fromFloor; i <= toFloor; i++) {
-//                ElevatorDisplay.getInstance().updateElevator(elevNum, i, numRiders, UP);
-//                Thread.sleep(80);
-//            }
-//        } else {
-//            for (int i = fromFloor; i >= toFloor; i--) {
-//                ElevatorDisplay.getInstance().updateElevator(elevNum, i, numRiders, DOWN);
-//                Thread.sleep(80);
-//            }
-//        }
-//        ElevatorDisplay.getInstance().openDoors(elevNum);
-//    }
+    private static void moveElevator(int elevNum, int fromFloor, int toFloor) throws InterruptedException {
+        int numRiders = (int) (11.0 * Math.random()) + 1;
+
+        ElevatorDisplay.getInstance().closeDoors(elevNum);
+        if (fromFloor < toFloor) {
+            for (int i = fromFloor; i <= toFloor; i++) {
+            	Direction UP = ElevatorDisplay.Direction.UP;
+            	ElevatorDisplay.getInstance().updateElevator(elevNum, i, numRiders, UP);
+                Thread.sleep(80);
+            }
+        } else {
+            for (int i = fromFloor; i >= toFloor; i--) {
+            	Direction DOWN = ElevatorDisplay.Direction.DOWN;
+               ElevatorDisplay.getInstance().updateElevator(elevNum, i, numRiders, DOWN);
+                Thread.sleep(80);
+            }
+       }
+        ElevatorDisplay.getInstance().openDoors(elevNum);
+   }
 }
